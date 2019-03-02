e03f9e12d11755eaff40c8516b877df273c2d7b6
Alice Fang <alicesf2@illinois.edu>
2019-03-02 01:08:24 -0600

    updated user model

--- models/user.js
+++ models/user.js
@@ -1,9 +1,21 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
-var UserSchema = new mongoose.Schema({
+/*
+  Schema for User db objects
+  netid: Illinois netid
+  points: event points for user (will be updated to also include office hour + committee points)
+  office_hours: array of all office hour dates attended
+  committees: array of all committee meeting dates attended
+*/
+
+var UserSchema = new mongoose.Schema(
+  {
     netid: String,
-    office_hours: { type: Array, default: []},
-    committees: { type: Array, default: []}},
-    { usePushEach: true});
+    points: Number,
+    office_hours: { type: Array, default: [] },
+    committees: { type: Array, default: [] }
+  },
+  { usePushEach: true }
+);
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
