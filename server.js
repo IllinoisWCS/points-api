8251e025ef17ad597865154c4807826215b61268
annabuyevich <avbuyevich@gmail.com>
2019-09-07 19:03:53 -0500

    trying to fix cors issue

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // Get the packages we need
 var express = require("express"),
-  router = express.Router(),
-  mongoose = require("mongoose"),
-  secrets = require("./config/secrets"),
-  bodyParser = require("body-parser"),
-  cors = require("cors");
+    router = express.Router(),
+    mongoose = require("mongoose"),
+    secrets = require("./config/secrets"),
+    bodyParser = require("body-parser"),
+    cors = require("cors");
 
 var app = express();
 app.use(cors());
@@ -14,27 +14,26 @@ var port = process.env.PORT || 3000;
 
 // Connect to a MongoDB
 mongoose.connect(
-  secrets.mongo_connection,
-  { useMongoClient: true }
+    secrets.mongo_connection, { useMongoClient: true }
 );
 
 //Allow CORS so that backend and frontend could be put on different servers
-var allowCrossDomain = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-  next();
-};
-app.use(allowCrossDomain);
-
+// var allowCrossDomain = function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+//   next();
+// };
+// app.use(allowCrossDomain);
+app.use(cors());
 // Use the body-parser package in our application
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+    bodyParser.urlencoded({
+        extended: true
+    })
 );
 app.use(bodyParser.json());
 
