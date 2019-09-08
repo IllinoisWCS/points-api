31d3b00c7060d5b35439b19d25a68b75f2583840
annabuyevich <avbuyevich@gmail.com>
2019-09-07 19:10:01 -0500

    trying to figure out cors

--- server.js
+++ server.js
@@ -28,7 +28,21 @@ mongoose.connect(
 //   next();
 // };
 // app.use(allowCrossDomain);
-app.use(cors());
+app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+
+    //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    // Pass to next layer of middleware
+    next();
+});
 // Use the body-parser package in our application
 app.use(
     bodyParser.urlencoded({
