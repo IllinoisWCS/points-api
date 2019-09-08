20b1e2ba3f238ff72417b0a63c5cee995746e31d
annabuyevich <avbuyevich@gmail.com>
2019-09-07 19:56:50 -0500

    trying to figure out cors

--- server.js
+++ server.js
@@ -19,13 +19,17 @@ mongoose.connect(
 
 //Allow CORS so that backend and frontend could be put on different servers
 var allowCrossDomain = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header(
+    //     "Access-Control-Allow-Headers",
+    //     "Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+    // );
+    // res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    // next();
 };
 app.use(allowCrossDomain);
 // app.use(function(req, res, next) {
