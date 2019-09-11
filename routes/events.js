bdf105be0ceef47b05e973c116d828d4a55e9ffe
annabuyevich <avbuyevich@gmail.com>
2019-09-10 23:11:56 -0500

    added try catch

--- routes/events.js
+++ routes/events.js
@@ -10,87 +10,103 @@ module.exports = function(router) {
 
     // get all events
     eventsRoute.get(async(req, res) => {
-        const events = await Event.find({});
-        res.json({
-            code: 200,
-            result: events,
-            success: true,
-        });
+        try {
+            const events = await Event.find({});
+            res.json({
+                code: 200,
+                result: events,
+                success: true,
+            });
+        } catch (err) {
+            console.log(err)
+        }
     })
 
     // get one event
     eventIdRoute.get(async(req, res) => {
-        const eventId = req.params.event_id
-        const event = await Event.findById(eventId)
-        res.json({
-            code: 200,
-            result: event,
-            success: true,
-        })
+        try {
+            const eventId = req.params.event_id
+            const event = await Event.findById(eventId)
+            res.json({
+                code: 200,
+                result: event,
+                success: true,
+            })
+
+        } catch (err) {
+            console.log(err)
+        }
+
     })
 
     // create new event
     eventsRoute.post(async(req, res) => {
+        try {
+            const data = req.body
+            console.log("data in post:" + data.points)
+            const eventKey = utils.generateEventKey()
+            let errMsg = ''
 
-        const data = req.body
-            // console.log("data in post:" + data.points)
-        const eventKey = utils.generateEventKey()
-        let errMsg = ''
+            if (!data.name) {
+                errMsg += 'Name, '
+            }
+            if (!data.points) {
+                errMsg += 'Points, '
+            }
+            if (!data.category) {
+                errMsg += 'Category, '
+            }
+            if (!data.date) {
+                errMsg += 'Date, '
+            }
+            if (!data.startTime) {
+                errMsg += 'Start Time, '
+            }
+            if (!data.endTime) {
+                errMsg += 'End Time, '
+            }
+            if (!data.password) {
+                errMsg += 'Password, '
+            }
+            if (errMsg) {
 
-        if (!data.name) {
-            errMsg += 'Name, '
-        }
-        if (!data.points) {
-            errMsg += 'Points, '
-        }
-        if (!data.category) {
-            errMsg += 'Category, '
-        }
-        if (!data.date) {
-            errMsg += 'Date, '
-        }
-        if (!data.startTime) {
-            errMsg += 'Start Time, '
-        }
-        if (!data.endTime) {
-            errMsg += 'End Time, '
-        }
-        if (!data.password) {
-            errMsg += 'Password, '
-        }
-        if (errMsg) {
+                errMsg += 'is required.'
+                res.json({
+                    code: 500,
+                    message: errMsg,
+                    success: false,
+                })
+            } else if (!utils.validateUser(data.password)) {
+                res.json({
+                    code: 404,
+                    message: 'You are not authorized to create events.',
+                    success: false,
+                })
+            } else {
 
-            errMsg += 'is required.'
-            res.json({
-                code: 500,
-                message: errMsg,
-                success: false,
-            })
-        } else if (!utils.validateUser(data.password)) {
-            res.json({
-                code: 404,
-                message: 'You are not authorized to create events.',
-                success: false,
-            })
-        } else {
+                const newEvent = new Event({
+                    name: data.name,
+                    points: data.points,
+                    category: data.category,
+                    key: eventKey,
+                    date: data.date,
+                    startTime: data.startTime,
+                    endTime: data.endTime,
+                });
+                await newEvent.save()
+                res.json({
+                    code: 200,
+                    message: 'Event Successfully Created',
+                    result: eventKey,
+                    success: true,
+                })
+            }
 
-            const newEvent = new Event({
-                name: data.name,
-                points: data.points,
-                category: data.category,
-                key: eventKey,
-                date: data.date,
-                startTime: data.startTime,
-                endTime: data.endTime,
-            });
-            await newEvent.save()
-            res.json({
-                code: 200,
-                message: 'Event Successfully Created',
-                result: eventKey,
-                success: true,
-            })
+        } catch (err) {
+            console.log(err)
         }
+
+
     })
 
     // update an event
@@ -142,103 +158,122 @@ module.exports = function(router) {
 
     // check-in to event
     eventIdRoute.put(async(req, res) => {
-        const data = req.body
-            // console.log(data)
-        const netId = data.netid
-        const key = data.event_key
-        const eventId = data.event_id
-            // const eventPoints = data.points
-            // console.log("point for this event:" + eventPoints)
+        try {
+            const data = req.body
+                // console.log(data)
+            const netId = data.netid
+            const key = data.event_key
+            const eventId = data.event_id
+                // const eventPoints = data.points
+                // console.log("point for this event:" + eventPoints)
 
-        // invalid netid
-        const isValid = utils.validateNetid(netId)
-            // const isValid = true;
-        const event = await Event.findById(eventId)
-            // console.log(utils.validateTime(event))
-        if (!isValid) {
-            res.json({
-                code: 404,
-                message: 'Invalid Netid',
-                success: false,
-            })
-        } else if (!event) {
-            res.json({
-                code: 404,
-                message: 'Invalid Event',
-                success: false,
-            })
-        } else if (key !== event.key) {
-            res.json({
-                code: 404,
-                message: 'Invalid Event Key',
-                success: false,
-            })
-        } else if (!utils.validateTime(event)) {
-            // console.log("WRONG TIME");
-            res.json({
-                code: 404,
-                message: 'Event Not Running',
-                success: false,
-            })
-        } else if (event.attendees.includes(netId)) {
-            res.json({
-                code: 200,
-                message: 'Already Signed In',
-                success: true,
-            })
-        } else {
-            event.attendees.push(netId)
-            await event.save()
-
-            // update user
-            let user = await User.findOne({
-                netId: data.netId
-            })
-            if (!user) {
-                const newUser = new User({
-                    netId,
+            // invalid netid
+            const isValid = utils.validateNetid(netId)
+                // const isValid = true;
+            const event = await Event.findById(eventId)
+                // console.log(utils.validateTime(event))
+            if (!isValid) {
+                res.json({
+                    code: 404,
+                    message: 'Invalid Netid',
+                    success: false,
                 })
-                user = newUser
+            } else if (!event) {
+                res.json({
+                    code: 404,
+                    message: 'Invalid Event',
+                    success: false,
+                })
+            } else if (key !== event.key) {
+                res.json({
+                    code: 404,
+                    message: 'Invalid Event Key',
+                    success: false,
+                })
+            } else if (!utils.validateTime(event)) {
+                // console.log("WRONG TIME");
+                res.json({
+                    code: 404,
+                    message: 'Event Not Running',
+                    success: false,
+                })
+            } else if (event.attendees.includes(netId)) {
+                res.json({
+                    code: 200,
+                    message: 'Already Signed In',
+                    success: true,
+                })
+            } else {
+                event.attendees.push(netId)
+                await event.save()
+
+                // update user
+                let user = await User.findOne({
+                    netId: data.netId
+                })
+                if (!user) {
+                    const newUser = new User({
+                        netId,
+                    })
+                    user = newUser
+                }
+
+
+                // user.points += eventPoints
+                await user.save()
+
+                res.json({
+                    code: 200,
+                    message: 'Successfully Checked In',
+                    success: true,
+                })
             }
 
-
-            // user.points += eventPoints
-            await user.save()
-
-            res.json({
-                code: 200,
-                message: 'Successfully Checked In',
-                success: true,
-            })
+        } catch (err) {
+            console.log(err)
         }
+
     })
 
     // delete one event
     eventIdRoute.delete(async(req, res) => {
-        const eventId = req.params.event_id
-        const event = await Event.findByIdAndRemove(eventId)
+        try {
+            const eventId = req.params.event_id
+            const event = await Event.findByIdAndRemove(eventId)
 
-        const ret = event ? {
-            code: 200,
-            message: 'Event Deleted Successfully',
-            success: true,
-        } : {
-            code: 404,
-            message: 'Event Not Found',
-            success: false,
+            const ret = event ? {
+                code: 200,
+                message: 'Event Deleted Successfully',
+                success: true,
+            } : {
+                code: 404,
+                message: 'Event Not Found',
+                success: false,
+            }
+
+            res.json(ret)
+
+        } catch (err) {
+            console.log(err)
         }
 
-        res.json(ret)
     })
 
     // delete all events
     eventsRoute.delete(async(req, res) => {
+        try {
+
+        } catch (err) {
+            console.log(err)
+        }
         await Event.deleteMany({})
+
         res.json({
             code: 200,
             message: 'Events Deleted Successfully',
             success: true,
         })
+
     })
 
     return router
--- routes/users.js
+++ routes/users.js
@@ -8,151 +8,187 @@ module.exports = function(router) {
 
     // get all users
     usersRoute.get(async(req, res) => {
-        const users = await User.find({})
-        res.json({
-            code: 200,
-            result: users,
-            success: true,
-        })
+        try {
+            const users = await User.find({})
+            res.json({
+                code: 200,
+                result: users,
+                success: true,
+            })
+
+        } catch (err) {
+            console.log(err)
+        }
+
     })
 
     // get one user
     netIdRoute.get(async(req, res) => {
-        const netid = req.params.net_id
-            // console.log("Inside User: " + req.params)
-        const user = await User.findOne({
-                netId: netid
+        try {
+            const netid = req.params.net_id
+                // console.log("Inside User: " + req.params)
+            const user = await User.findOne({
+                    netId: netid
+                })
+                // console.log("user found: " + user)
+            res.json({
+                code: 200,
+                result: user,
+                success: true,
             })
-            // console.log("user found: " + user)
-        res.json({
-            code: 200,
-            result: user,
-            success: true,
-        })
+
+        } catch (err) {
+            console.log(err)
+        }
+
     })
 
     // create new user
     usersRoute.post(async(req, res) => {
-        const data = req.body
-        const netId = data.netId
-            // console.log(data);
+        try {
+            const data = req.body
+            const netId = data.netId
+                // console.log(data);
 
-        if (!netId) {
-            res.json({
-                code: 404,
-                message: 'NetId is Required',
-                success: false,
-            })
-        } else if (!utils.validateNetid(netId)) {
-            res.json({
-                code: 404,
-                message: 'Invalid NetId',
-                success: false,
-            })
-        } else {
-            let user = await User.findOne({ netId })
-            if (!user) {
-                const newUser = new User({ netId });
-                await newUser.save()
-                res.json({
-                    code: 200,
-                    message: 'User Successfully Created',
-                    success: true,
-                })
-            } else {
+            if (!netId) {
                 res.json({
                     code: 404,
-                    message: 'User Already Exists',
+                    message: 'NetId is Required',
                     success: false,
                 })
+            } else if (!utils.validateNetid(netId)) {
+                res.json({
+                    code: 404,
+                    message: 'Invalid NetId',
+                    success: false,
+                })
+            } else {
+                let user = await User.findOne({ netId })
+                if (!user) {
+                    const newUser = new User({ netId });
+                    await newUser.save()
+                    res.json({
+                        code: 200,
+                        message: 'User Successfully Created',
+                        success: true,
+                    })
+                } else {
+                    res.json({
+                        code: 404,
+                        message: 'User Already Exists',
+                        success: false,
+                    })
+                }
             }
+
+        } catch (err) {
+            console.log(err)
         }
+
     })
 
     // update user committee, oh, gwc
     netIdRoute.put(async(req, res) => {
-        const data = req.body
-        const netId = req.params.net_id
-        const user = await User.findOne({ netId })
-        let message = 'Checked in!'
-            // console.log(data)
-            // console.log(netId)
+        try {
+            const data = req.body
+            const netId = req.params.net_id
+            const user = await User.findOne({ netId })
+            let message = 'Checked in!'
+                // console.log(data)
+                // console.log(netId)
 
-        if (!user) {
-            const newUser = new User({ netId });
-            await newUser.save()
-            user = newUser
-        }
-        if (data.key) {
-            const event = await Event.findOne({ key: data.key })
-            if (user.attendedEvents.indexOf(data.key) <= -1) {
-                user.points += event.points
-                user.attendedEvents.push(data.key)
-
+            if (!user) {
+                const newUser = new User({ netId });
+                await newUser.save()
+                user = newUser
             }
+            if (data.key) {
+                const event = await Event.findOne({ key: data.key })
+                if (user.attendedEvents.indexOf(data.key) <= -1) {
+                    user.points += event.points
+                    user.attendedEvents.push(data.key)
+
+                }
+            }
+
+            // if (data.type === 'committee') {
+            //     if (!user.committees.includes(data.date)) {
+            //         user.committees.push(data.date)
+            //         user.points += 0.5
+            //         message = 'Successfully Added Committee'
+            //     }
+            // } else if (data.type === 'officeHours') {
+            //     if (!user.officeHours.includes(data.date)) {
+            //         user.officeHours.push(data.date)
+            //         user.points += 0.5
+            //         message = 'Successfully Added Office Hour'
+            //     }
+            // } else if (data.type === 'girlsWhoCode') {
+            //     if (!user.girlsWhoCode.includes(data.date)) {
+            //         user.girlsWhoCode.push(data.date)
+            //         user.points += 0.5
+            //         message = 'Successfully Added Girls Who Code'
+            //     }
+            // } else if (data.type === 'attendedEvents') { // TODO: UPDATED CHECK!!
+            //     if (!user.attendedEvents.includes(data.date)) {
+            //         user.attendedEvents.push(data.date)
+            //         user.points += 0.5
+            //         message = 'Successfully Added Attended Event'
+            //     }
+            // }
+
+            await user.save()
+            res.json({
+                code: 200,
+                message: message,
+                result: user.attendedEvents,
+                success: true,
+            })
+
+        } catch (err) {
+            console.log(err)
         }
 
-        // if (data.type === 'committee') {
-        //     if (!user.committees.includes(data.date)) {
-        //         user.committees.push(data.date)
-        //         user.points += 0.5
-        //         message = 'Successfully Added Committee'
-        //     }
-        // } else if (data.type === 'officeHours') {
-        //     if (!user.officeHours.includes(data.date)) {
-        //         user.officeHours.push(data.date)
-        //         user.points += 0.5
-        //         message = 'Successfully Added Office Hour'
-        //     }
-        // } else if (data.type === 'girlsWhoCode') {
-        //     if (!user.girlsWhoCode.includes(data.date)) {
-        //         user.girlsWhoCode.push(data.date)
-        //         user.points += 0.5
-        //         message = 'Successfully Added Girls Who Code'
-        //     }
-        // } else if (data.type === 'attendedEvents') { // TODO: UPDATED CHECK!!
-        //     if (!user.attendedEvents.includes(data.date)) {
-        //         user.attendedEvents.push(data.date)
-        //         user.points += 0.5
-        //         message = 'Successfully Added Attended Event'
-        //     }
-        // }
-
-        await user.save()
-        res.json({
-            code: 200,
-            message: message,
-            result: user.attendedEvents,
-            success: true,
-        })
     })
 
     // delete user
     netIdRoute.delete(async(req, res) => {
-        const netId = req.params.net_id
-        const user = await User.findOneAndRemove({ netId })
+        try {
+            const netId = req.params.net_id
+            const user = await User.findOneAndRemove({ netId })
 
-        const ret = user ? {
-            code: 200,
-            message: 'User Deleted Successfully',
-            success: true,
-        } : {
-            code: 404,
-            message: 'User Not Found',
-            success: false,
+            const ret = user ? {
+                code: 200,
+                message: 'User Deleted Successfully',
+                success: true,
+            } : {
+                code: 404,
+                message: 'User Not Found',
+                success: false,
+            }
+
+            res.json(ret)
+
+        } catch (err) {
+            console.log(err)
         }
 
-        res.json(ret)
     })
 
     // delete all users
     usersRoute.delete(async(req, res) => {
-        await User.deleteMany({})
-        res.json({
-            code: 200,
-            message: 'Users Deleted Successfully',
-            success: true,
-        })
+        try {
+            await User.deleteMany({})
+            res.json({
+                code: 200,
+                message: 'Users Deleted Successfully',
+                success: true,
+            })
+
+        } catch (err) {
+            console.log(err)
+        }
+
     })
 
     return router
