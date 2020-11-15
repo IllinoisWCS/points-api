2fb23ef93ec1b63981b484b062fc9bd6106f4058
Eunice Zhou <eunicexzhou@gmail.com>
2020-11-15 16:55:18 -0600

    enable cors

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ var express = require("express"),
     cors = require("cors");
 
 var app = express();
+
+// enable cors
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 // app.use(cors());
 
 // Use environment defined port or 3000
