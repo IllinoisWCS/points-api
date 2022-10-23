276dcbcf96bd288b70056a0a2c03969208dc9a17
Daniel Wang <hello@danielwang.dev>
2022-10-22 20:42:36 -0500

    Remove API prefix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", require("./routes/"));
+app.use("/", require("./routes/"));
 
 const port = process.env.PORT || 3000;
 
