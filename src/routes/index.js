e3d7484cbac649b0749086eded2510903bdb3e19
Daniel Wang <hello@danielwang.dev>
2022-10-24 20:02:47 -0500

    Add route to list all users

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const isAuthenticated = require("../middlewares/isAuthenticated");
+const isOfficer = require("../middlewares/isOfficer");
 
 router.use(/\/auth/, require("./auth"));
 router.use("/events", isAuthenticated, require("./events"));
 router.use("/user", isAuthenticated, require("./user"));
+router.use("/users", isAuthenticated, isOfficer, require("./users"));
 
 router.use((_req, res) => {
   res.sendStatus(404);
---
+++ src/routes/users.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const User = require("../models/user");
+
+router.get("/", async (req, res, next) => {
+  User.find({})
+    .populate("events")
+    .exec(function (err, result) {
+      if (err) return next(err);
+
+      if (result) {
+        res.status(200).send(result);
+      } else {
+        res.sendStatus(404);
+      }
+    });
+});
+
+module.exports = router;
