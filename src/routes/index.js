89bc7214cb4e61aab3451f7eea1eedc97419dff1
Daniel Wang <hello@danielwang.dev>
2022-10-27 16:58:22 -0500

    Rename user endpoint to profile

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const isOfficer = require("../middlewares/isOfficer");
 
 router.use(/\/auth/, require("./auth"));
 router.use("/events", require("./events"));
-router.use("/user", isAuthenticated, require("./user"));
+router.use("/profile", isAuthenticated, require("./profile"));
 router.use("/users", isAuthenticated, isOfficer, require("./users"));
 
 router.use((_req, res) => {
