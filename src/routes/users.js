f7022025470ee1f5dc2bb974c813ce65117a0b7b
Daniel Wang <hello@danielwang.dev>
2022-10-28 00:13:34 -0500

    Add route to query a single user

--- src/routes/users.js
+++ src/routes/users.js
@@ -15,4 +15,18 @@ router.get("/", async (req, res, next) => {
     });
 });
 
+router.get("/:netId", async (req, res, next) => {
+  User.find({ netId: req.params.netId })
+    .populate("events")
+    .exec(function (err, result) {
+      if (err) return next(err);
+
+      if (result) {
+        res.status(200).send(result);
+      } else {
+        res.sendStatus(404);
+      }
+    });
+});
+
 module.exports = router;
