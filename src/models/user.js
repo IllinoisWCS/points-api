51e7082267bc78323a11a23bf3623067c65bed81
Daniel Wang <hello@danielwang.dev>
2022-12-28 15:48:23 -0500

    Change role enum to isOfficer boolean

--- src/models/user.js
+++ src/models/user.js
@@ -7,11 +7,7 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
-  role: {
-    type: String,
-    default: "member",
-    enum: ["member", "committee", "officer"],
-  },
+  isOfficer: { type: Boolean, default: false },
   events: [{ type: mongoose.ObjectId, ref: "Event" }],
   points: { type: Number, default: 0, min: 0 },
 });
