0e6870a13c0d5e3fd83ba43185fee352af52f7c8
Daniel Wang <hello@danielwang.dev>
2023-01-24 18:06:59 -0600

    refactor: remove unused import

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import express, { Express } from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
 import MongoStore from 'connect-mongo';
 import passport from 'passport';
