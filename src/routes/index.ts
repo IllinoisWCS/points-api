e6a116d0ab3aaa2b60daa8441fccc9508414f629
aliyaa23 <71795590+aliyaa23@users.noreply.github.com>
2026-04-14 11:07:14 -0500

    Redeem logic (#42)
    * PointBar and {name's} point tracker added when user is logged in
    
    * redeeming checkpoint logic backend
    
    * uncommenting important routes
    
    ---------
    
    Co-authored-by: helenailic <helenailic278@gmail.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import { eventsRoute } from './events';
 import { profileRoute } from './profile';
 import { usersRoute } from './users';
 import { checkpointsRoute } from './checkpoints';
+import { vintageRoute } from './vintage';
 
 export const routes = express.Router();
 
@@ -13,6 +14,7 @@ routes.use('/events', eventsRoute);
 routes.use('/profile', isAuthenticated, profileRoute);
 routes.use('/users', isAuthenticated, usersRoute);
 routes.use('/checkpoints', checkpointsRoute);
+routes.use('/vintage', vintageRoute);
 
 routes.use((_req, res) => {
   res.sendStatus(404);
