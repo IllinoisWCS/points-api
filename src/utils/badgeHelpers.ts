a9d9c6263601156e29165d22de9aadcb74d6669e
helenailic <helenailic278@gmail.com>
2026-04-22 13:53:24 -0500

    awaint await premise properly

--- src/utils/badgeHelpers.ts
+++ src/utils/badgeHelpers.ts
@@ -1,21 +1,43 @@
 import Badge from '../models/badge';
 
-export async function checkTieredBadge(user: User, counterField: string, tier1: number, tier2: number, tier3: number) {
-    if (user[counterField] >= tier3) {
-        let badge = await Badge.findOne({isTiered: true, category: counterField, tier: 3}).name;
-        if (user.badges.includes(badge) == false) {
-            return badge;
-        }
-    } else if (user[counterField] >= tier2) {
-        let badge = await Badge.findOne({isTiered: true, category: counterField, tier: 2}).name;
-        if (user.badges.includes(badge) == false) {
-            return badge;
-        }
-    } else if (user[counterField] >= tier1) {
-        let badge = await Badge.findOne({isTiered: true, category: counterField, tier: 1}).name;
-        if (user.badges.includes(badge) == false) {
-            return badge;
-        }
+export async function checkTieredBadge(
+  user: User,
+  counterField: string,
+  tier1: number,
+  tier2: number,
+  tier3: number
+) {
+  if (user[counterField] >= tier3) {
+    const badge = await Badge.findOne({
+      isTiered: true,
+      category: counterField,
+      tier: 3
+    });
+
+    if (badge && !user.badges.includes(badge.name)) {
+      return badge.name;
     }
-    return null;
-};
+  } else if (user[counterField] >= tier2) {
+    const badge = await Badge.findOne({
+      isTiered: true,
+      category: counterField,
+      tier: 2
+    });
+
+    if (badge && !user.badges.includes(badge.name)) {
+      return badge.name;
+    }
+  } else if (user[counterField] >= tier1) {
+    const badge = await Badge.findOne({
+      isTiered: true,
+      category: counterField,
+      tier: 1
+    });
+
+    if (badge && !user.badges.includes(badge.name)) {
+      return badge.name;
+    }
+  }
+
+  return null;
+}
